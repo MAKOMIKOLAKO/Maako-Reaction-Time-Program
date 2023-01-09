4107d9fc20bb5e5ece9b6a153ea9c1c24c14eefd
MAKOMIKOLAKO <reachmaako@gmail.com>
2023-01-09 21:49:07 +0100

    added comments

--- Reaction Time Program.py
+++ Reaction Time Program.py
@@ -4,14 +4,20 @@ import pickle
 import time
 import random
 
+# Color scheme of the program
 darkblue = [61, 90, 128]
 midblue = [152, 193, 217]
 lightblue = [224, 251, 252]
 orange = [238, 108, 77]
 black = [41, 50, 65]
+
 ibm = 'IBMPlexSans-SemiBoldItalic.ttf'
+
+# Attributes for the app that each of the windows will inherit
 app = App(width=1000, height=700, bg=darkblue)
 app.font = ibm
+
+# Variables used in each of the windows
 students = []
 numStudents = len(students)
 runs = 0
@@ -19,12 +25,14 @@ haschanged = False
 programend=False
 buttoncolorchange = time.time()
 
+# Student object - each of the students and their data is saved this way
 class Student():
     #studentNumber = 0
     age = 0
     bestTime = 0
     allTimes = []
 
+# Takes the current 'students' list and saves it as a .pickle file
 def saveClose():
     global students, ibm, fileTo
     window = Window(app, title='Save Data to File')
@@ -36,12 +44,16 @@ def saveClose():
     def do_this():
         global fileTo
         global students, saveConfirm, fileTo
+        # Takes the value of the text box on the button press and uses it as the file name
         store = fileTo.value+'.pickle'
         with open(store, 'wb') as outfile:
             pickle.dump(students, outfile)
+        # Confirmation message
         saveConfirm = app.info(app, text=f'Data has been saved as {store}')
     submit1.when_clicked = do_this
 
+# Takes the user's input and looks for a .pickle file of the same name
+# to overwrite the current 'students' list
 def openAFile():
     global students, ibm
     window = Window(app, title='Open Data from File', bg=darkblue)
@@ -54,6 +66,7 @@ def openAFile():
                 students = pickle.load(infile)
                 fileConfirm.visible=True
         except:
+            # Exception handler for if the user's input does not correspond to any saved file
             notFound=Text(window, text='File not found.')
     userHelp = Text(window, text='Enter file name (case sensitive): ')
     fileFrom = TextBox(window, width=150)
@@ -77,6 +90,7 @@ def runTest():
         def buttonpress():
             global haschanged, runs, programend, currenttime, buttoncolorchange
             if haschanged:
+                # Will only run if the button color has changed. If not, the user's click will not allow them to progress.
                 finaltime=round(time.time()-buttoncolorchange, 3)*1000
                 window.warn(window, text=f'Your reaction time: {finaltime} ms')
                 list_of_results.append(finaltime)
@@ -97,6 +111,7 @@ def runTest():
                     haschanged = False
                     testprocedure()
             else:
+                # If the user clicks and the color has not changed, they are given a warning and they have to restart the current trial.
                 window.warn('Warning','Button has not yet changed color')
                 testprocedure()
 
@@ -104,8 +119,8 @@ def runTest():
             global haschanged, buttoncolorchange
             try:
                 timer.value = int(timer.value) + 1
+                # If the predetermined amount of time has passed, the color of the button will change.
                 if int(timer.value) == int(chosentime):
-                    # Need a variable to record if button has been destroyed
                     react.bg=[238, 108, 77]
                     haschanged=True
                     buttoncolorchange=time.time()
@@ -127,6 +142,7 @@ def runTest():
 
         def newStudent():
             global typeName, ageinput
+            # Once the 3 trials have been completed, the student can then input their data.
             guide = Text(window, text='Input Student Name:')
             typeName = TextBox(window, width=100)
             guide2 = Text(window, text='Input Student Age:')
@@ -141,12 +157,17 @@ def openleaderboard():
     global students
     leaderBoard = Window(app, title='Leaderboard', height=700, width=1000, bg=darkblue)
     times = students
+    # Takes all the students in the system and sorts them by time.
+    # Then it outputs the top 10 school-wide.
     title=Text(leaderBoard, text='Top 10 Times School-Wide:', size=50)
     def getstudentinfo(student):
+        # Takes the student's bestTime attribute and returns it.
         return student.bestTime
+    # I can use the function to sort the terms in the 'times' list by the bestTime attribute.
     times.sort(key=getstudentinfo)
     scores = Text(leaderBoard, text='', visible=False, size=22)
     pos = 1
+    # Sets a limit so only the first 10 times in the sorted list are printed.
     for j in times:
         if pos==11:
             break
@@ -154,12 +175,18 @@ def openleaderboard():
         pos+=1
 
     def visualiser():
+        # I use matplotlib to display the correlation between age and reaction time.
+        # x-axis data
         ages = []
+        # y-axis data
         xtimes = []
         for i in times:
+            # Splits the list of students and takes only their age and bestTime (because this is all that will be plotted)
             ages.append(int(i.age))
             xtimes.append(int(i.bestTime))
+        # Scatter plot object
         plt.scatter(ages, xtimes)
+        # Axis titles
         plt.title("Age vs Reaction Time")
         plt.xlabel("Age")
         plt.ylabel("Reaction Time (ms)")
@@ -167,6 +194,8 @@ def openleaderboard():
     displayresultsbutton = PushButton(leaderBoard, text='Visualise Data', command=visualiser)
 
     def agegroupstats(sv):
+        # Takes the top 10 times of any age group.
+        # Probably more complicated than it needed to be
         agegroups=Box(leaderBoard, visible=False, height=200, width=400)
         agegroups.bg=orange
         includedstudents=[]
@@ -178,9 +207,11 @@ def openleaderboard():
         for i in includedstudents:
             for x in i.allTimes:
                 includedtimes.append(x)
+        # Gives the best time and average time of the year group.
         agestats=Text(agegroups, text=f'Stats for age {sv}:\nAverage time: {int(sum(includedtimes)/len(includedtimes))}ms\nBest time: {min(includedtimes)}\nWorst time: {max(includedtimes)}')
         displaystudents=Text(agegroups, text=f'Age {sv} leaderboard:\n', visible=False)
         counter=1
+        # Counter so only the first 10 times are shown.
         for x in includedstudents:
             displaystudents.append(f'{counter}. {x.name} - {x.bestTime}\n')
             counter+=1
@@ -189,6 +220,7 @@ def openleaderboard():
         displaystudents.visible=True
         agegroups.visible=True
         def destroy():
+            # When the 'close' button is pressed, the mini box with the year-group specific data is closed.
             agegroups.destroy()
             closeall.destroy()
         closeall = PushButton(leaderBoard, command=destroy, text='close')
@@ -196,14 +228,17 @@ def openleaderboard():
     multiselect=Combo(leaderBoard, options=[12, 13, 14, 15, 16, 17, 18], command=agegroupstats)
 
 def bruh():
+    # Helped for figuring out what was wrong when something broke
     for i in students:
         print(f'{i.name} is {i.age} and their best time is {i.bestTime}ms')
 
+# Menu screen buttons for each of the functions
 title = Text(app, text='Menu', size=100, color=orange)
 spacer = Box(app, height=100, width=100)
 testbutton = PushButton(app, text='Test Reaction Time', command=runTest, image='reactiontimetest.png')
 viewleaderboard = PushButton(app, text='View Leaderboard', command=openleaderboard)
 savenclosebutton = PushButton(app, text='Save Data to File', command=saveClose, image='savenclose.png')
 saveopenbutton = PushButton(app, text='Open Data from File', command=openAFile, image='openfromfile.png')
+# This was just to help me figure out what wasn't working when something broke
 #debugbutton=PushButton(app, text='frick', command=bruh)
 app.display()
